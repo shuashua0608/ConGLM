09ed0f2299f81b1956acc587c2ca082e1c1184c8
shuashua0608 <74279976+shuashua0608@users.noreply.github.com>
2022-12-27 15:20:30 +0800

    Create init.py

---
+++ init.py
@@ -0,0 +1,4 @@
+linucb_para={'lambda':1,'sigma':0.05, 'alpha': 0.5}
+conucb_para = {'lambda':0.25, 'sigma':0.05, 'tilde_lambda':1, 'alpha': 0.25, 'tilde_alpha': 0.25}
+conduelucb_para = {'lamb':0.25, 'sigma':0.05, 'alpha': 0.25, 'kappa': 60}
+
