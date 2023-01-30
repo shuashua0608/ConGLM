df57704b70230f25856af93e1d6cb787dc09305b
shuaz <74279976+shuashua0608@users.noreply.github.com>
2023-01-30 10:44:17 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -13,9 +13,10 @@ arms = np.load(os.path.join(pth, "synthetic/arm_feats.npy"))
 suparms = np.load(os.path.join(pth, "synthetic/suparms.npy"))
 users = np.load(os.path.join(pth,"synthetic/users.npy"))
 
-horizon = 2000
+horizon = 5000
 pool_size = 20
 pool_index_list = pool_index(horizon, pool_size, arms)
+
 # conversation function #
 bt_log_5 = lambda t:5 * int(math.log(t + 1))
 bt_log_3 = lambda t:3 * int(math.log(t + 1))
@@ -25,7 +26,7 @@ bt_lin_5 = lambda t: int(0.5*t)
 bt_lin_3 = lambda t: int(0.3*t)
 bt_lin_1 = lambda t: int(0.1*t)
 
-theta_star = users[1]
+theta_star = users[0]
 info_maxinp = MaxInp(arms, maxinp_para, theta_star, pool_index_list, horizon)
 info_conglm = ConGLM(arms, suparms, conglm_para, theta_star, pool_index_list, bt_lin_1, horizon)
 
@@ -34,6 +35,7 @@ regret_conglm = info_conglm["regret"]
 
 plt.plot(regret_maxinp)
 plt.plot(regret_conglm)
+plt.legend(['maxinp','conglm'], loc = 'upper left')
 plt.show()
 
 
