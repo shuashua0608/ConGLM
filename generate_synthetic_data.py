2c23c7013d6ebb67d995fc2a17818c9c168f3b64
shuaz <74279976+shuashua0608@users.noreply.github.com>
2023-01-30 10:55:14 +0800

    Update generate_synthetic_data.py

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -17,15 +17,18 @@ def generate_dataset(dim=5, num_suparms=20, num_arms=100, num_users=100, M = 4):
     user_M = np.ones([num_users, dim])
 
     for i in range(num_arms):
-        X[i] = np.random.uniform(-1, 1, dim)
-        X[i] /= LA.norm(X[i])
+        X[i] = np.random.uniform(-1/np.sqrt(dim), 1/np.sqrt(dim), dim)
+        X[i]/=LA.norm(X[i])
+    for user_id in range(num_users):
+        user_M[user_id] = np.random.uniform(-1/np.sqrt(dim), 1/np.sqrt(dim), dim)
+        
 
     #### arm feature matrix ####
     suparm_X = np.ones([num_suparms, dim])
 
     for i in range(num_arms):
 
-        # mean_feat = np.zeros(dim)
+ 
         num_related_suparms = random.randint(1, M)
         related_suparms = random.sample(range(num_suparms), num_related_suparms)
 
@@ -41,9 +44,7 @@ def generate_dataset(dim=5, num_suparms=20, num_arms=100, num_users=100, M = 4):
                 fea_i = fea_i + X[j] / len(relation[j])
         suparm_X[i] = fea_i / ni
 
-        #### generate user parameter matrix
-    for user_id in range(num_users):
-        user_M[user_id] = 2 * np.random.rand(dim) - 1
+        #### generate user parameter matrix   
 
     return [X, suparm_X, relation, user_M]
 
